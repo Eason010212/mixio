4a048cee627f8bf0cb79f13d53d7c8b132a6bdb1
Eason010212 <1371033826@qq.com>
2023-03-01 20:53:49 +0800

    pkg tools update

--- pkg_tools/linux-arm64-pkg.sh
+++ pkg_tools/linux-arm64-pkg.sh
@@ -1,3 +1,5 @@
+curpath=$(cd "$(dirname "$0")"; pwd)
+cd ${curpath}
 version="{
     \"version\":\"1.9.6\",
     \"platform\":\"linux-arm64\",
@@ -13,10 +15,16 @@ mkdir logs
 cd ../
 chmod -R 777 mixio
 cd mixio
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
 cd node_modules/cliff/node_modules/winston/lib/winston
 find -name 'common.js' | xargs perl -pi -e 's|target.padLevels|false|g'
 cd ../../../../../../
 cd src
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
 pkg -t node16-linux-arm64 package.json
 mv -f loader ../../mixio_linux_arm64/src/loader
 cp -r certs ../../mixio_linux_arm64/src
