f6e492fc218af524208e82a4e5072ce4270cc878
Eason010212 <1371033826@qq.com>
2024-06-29 14:10:46 +0800

    colorful-bulb

--- js/widgets.js
+++ js/widgets.js
@@ -1727,7 +1727,7 @@ function add_bulb(user_title, user_topic, user_content, user_style, title_style)
         bulb.attr('class', 'bulb1')
     else if (user_content == 2)
         bulb.attr('class', 'bulb2')
-    else if (user_content == 2)
+    else if (user_content == 3)
         bulb.attr('class', 'bulb3')
     attrs = [
         ['user-type', 'output_bulb'],
@@ -1755,6 +1755,21 @@ function add_bulb(user_title, user_topic, user_content, user_style, title_style)
                 } else if (message1 == 3) {
                     title.parent().parent().attr('user-content', 3)
                     bulb.attr('class', 'bulb3')
+                }
+                else{
+                    var availableMessageStrings = [
+                        "红", "橙", "黄", "绿", "青", "蓝", "紫", "白", "黑", "灰", "棕",
+                        "red", "orange", "yellow", "green", "cyan", "blue", "purple", "white", "black", "grey", "brown"
+                    ]
+                    for (var avaMessage of availableMessageStrings) {
+                        console.log(message1.toString(), avaMessage)
+                        if (message1.toString()=== avaMessage) {
+                            title.parent().parent().attr('user-content', avaMessage);
+                            bulb.css('background-color', message1);
+                            bulb.css('box-shadow', message1 + ' 1px 1px 8px')
+                            break;
+                        }
+                    }
                 }
                 itemdiv.trigger(MixIO.eventTags.BULB_CHANGED, [Uint8ArrayToString(message1)])
             }
