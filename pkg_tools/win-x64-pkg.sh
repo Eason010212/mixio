59544e2c57f2a8905d1ae0b38063999e6bb3365b
fredqian <fredqian@bnu.edu.cn>
2025-12-22 10:17:01 +0800

    Update win-x64-pkg.sh

--- pkg_tools/win-x64-pkg.sh
+++ pkg_tools/win-x64-pkg.sh
@@ -6,7 +6,7 @@ mkdir mixio_win_x64
 chmod -R 777 mixio
 cd mixio
 npm install
-pkg -t node16-win-x64 package.json
+pkg -t node18-win-x64 package.json
 mv -f mixio ../mixio_win_x64/
 cd ../
 chmod -R 777 mixio_win_x64
